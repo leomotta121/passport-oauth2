ab68afb5ad25c802adeee96fe2d51af2313829e0
Leonardo Motta <mail@leomotta.me>
2019-09-06 10:50:16 -0300

    New example

--- google/server.js
+++ google/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleToken = require("passport-google-token").Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const cors = require("cors");
 
 const app = express();
@@ -9,17 +10,17 @@ app.use(express.json());
 app.use(cors());
 
 passport.use(
-  new GoogleStrategy(
+  new GoogleToken(
     {
       clientID:
         "479618265597-t0crngffvje8u3ro1qg2r6ept6qlavll.apps.googleusercontent.com",
       clientSecret: "3icaALSE-4xN6snqbMlnSCHP",
-      callbackURL: "http://localhost:3000/my-app"
+      callbackURL: "/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function(err, user) {
-        return cb(err, user);
-      });
+      console.log(profile);
+
+      return cb(null, profile);
     }
   )
 );
@@ -37,11 +38,36 @@ app.get(
 app.get(
   "/auth/google/callback",
 
-  passport.authenticate("google", { failureRedirect: "/login" }),
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:3000/login"
+  }),
 
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect("/");
+    res.redirect("http://localhost:3000/");
+  }
+);
+
+app.get(
+  "/private",
+  passport.authenticate("google-token", { session: false }),
+
+  function(req, res, next) {
+    if (!req.user) return res.status(401).json();
+
+    req.auth = {
+      id: req.user.id
+    };
+
+    return next();
+  },
+
+  function(req, res, next) {
+    return res.json({
+      message: "You are logged!",
+      auth: req.auth,
+      user: req.user
+    });
   }
 );
 
